1e82d024994a7aa17b86c98deca49784a40f6413
Nishana Banu <nishanabanu26@gmail.com>
2024-06-26 22:10:46 +0530

    first commit

---
+++ Frontend/shop_nest/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/navbar' element={<Navbar/>}/>
+      </Routes>
+          
+    </div>
+  );
+}
+
+export default App;
